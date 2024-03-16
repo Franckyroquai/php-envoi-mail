d95f015c84408aac6356b265c6581d14adf2ef40
Franckyroquai <franckyroquai@gmail.com>
2024-03-16 16:29:42 +0100

    préparation index

--- index.php
+++ index.php
@@ -7,24 +7,7 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <?php
-    echo"<h1>Envoi de mails en PHP</h1>";
-
-    $destinataire = "franckyroquai@gmail.com";
-    $titre = "Un message de mon application d'envoi de mails N°2 ;)";
-    $message = "Bonjour Franck
-    Ceci est un mail de confirmation de ton inscription
-    au club des champions !
-    Merci, nous sommes honorés";
-
-    $envoi = mail($destinataire,$titre, $message);
-
-    var_dump($envoi);
-
-
-
-
-    ?>
+   
     
 </body>
 </html>
