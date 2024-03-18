4c6920e3dd0e26d11317923b30d760f014c5ee37
Franckyroquai <franckyroquai@gmail.com>
2024-03-18 18:19:07 +0100

    ajout vérification des 4 champs complétés

--- lib/functions.php
+++ lib/functions.php
@@ -1,40 +1,49 @@
 <?php
 
     function check(){
+        $i=0;
         if(!empty($_REQUEST)) {
             /*Test données expéditeur*/
-            if (empty($_REQUEST["inputEmailExpediteur"])) {
+            if (empty(trim($_REQUEST["inputEmailExpediteur"]))) {
                 //Le cas où la variable inputEmailExpediteur est vide
                 $GLOBALS['errorExp'] = "Merci d'entrer l'email de l'expediteur";
             } else {
                 //Le cas contraire
-                $GLOBALS['valueExp'] = $_REQUEST["inputEmailExpediteur"];
+                $GLOBALS['valueExp'] = trim($_REQUEST["inputEmailExpediteur"]);
+                $i++;
             }
 
             /*Test données destinataire*/
-            if (empty($_REQUEST["inputEmailDestinataire"])) {
+            if (empty(trim($_REQUEST["inputEmailDestinataire"]))) {
                 //Le cas où la variable inputEmailExpediteur est vide
+
                 $GLOBALS['errorDes'] = "Merci d'entrer l'email du destinataire";
             } else {
                 //Le cas contraire
-                $GLOBALS['valueDes'] = $_REQUEST["inputEmailDestinataire"];
+                $GLOBALS['valueDes'] = trim($_REQUEST["inputEmailDestinataire"]);
+                $i++;
             }
             /*Test données objet*/
-            if (empty($_REQUEST["InputObjet"])) {
+            if (empty(trim($_REQUEST["InputObjet"]))) {
                 //Le cas où la variable inputEmailExpediteur est vide
                 $GLOBALS['errorObj'] = "Merci d'entrer l'objet de votre message";
             } else {
                 //Le cas contraire
-                $GLOBALS['valueObj'] = $_REQUEST["InputObjet"];
+                $GLOBALS['valueObj'] = trim($_REQUEST["InputObjet"]);
+                $i++;
             }
             /*Test données message*/
-            if (empty($_REQUEST["InputMessage"])) {
+            if (empty(trim($_REQUEST["InputMessage"]))) {
                 //Le cas où la variable inputEmailExpediteur est vide
                 $GLOBALS['errorMg'] = "Merci d'entrer votre message";
             } else {
                 //Le cas contraire
-                $GLOBALS['valueMg'] = $_REQUEST["InputMessage"];
+                $GLOBALS['valueMg'] = trim($_REQUEST["InputMessage"]);
+                $i++;
             }
+        }
+        if ($i==4) {
+            $GLOBALS['valid_data'] = true;
         }
     }
 
